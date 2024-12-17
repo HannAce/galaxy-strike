09b9e99beaaf4003c38dedd90af3e3690f97a1f4
Hannah Fisher <hannahtfisher@gmail.com>
2024-12-17 10:55:43 +0000

    Added clamping for movement boundaries, and player rotation on z axis when turning left and right (also removed a deprecated package for VS code editor)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,42 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float controlSpeed = 50f;
+    [SerializeField] private float xClampRange;
+    [SerializeField] private float yClampRange;
+
+    [SerializeField] private float controlRollFactor;
+    [SerializeField] private float controlPitchFactor;
+    [SerializeField] private float controlYawFactor;
+
+
 
     private Vector2 movement;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        float xOffset = movement.x * controlSpeed * Time.deltaTime;
-        float yOffset = movement.y * controlSpeed * Time.deltaTime;
-        
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+        ProcessTranslation();
+        ProcessRotation();
     }
 
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+        
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+        
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+    
+    private void ProcessRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, -controlRollFactor * movement.x);
+        transform.localRotation = targetRotation;
+    }
+    
     public void OnMove(InputValue value)
     {
         movement = value.Get<Vector2>();
